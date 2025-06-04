db2033dac7843a566fec2d6e9c4d5f9003fc0968
GollaRaju991 <rajkumar6714321@gmail.com>
2025-06-04 11:54:28 +0530

    Update locationData.ts
    I have updated Districts and division for ts and AP

--- src/data/locationData.ts
+++ src/data/locationData.ts
@@ -43,33 +43,71 @@ export const states = {
 
 export const districts = {
   TG: [
-    { code: 'HYD', name: 'Hyderabad' },
-    { code: 'WGL', name: 'Warangal' },
-    { code: 'NZB', name: 'Nizamabad' },
-    { code: 'KRM', name: 'Karimnagar' },
-    { code: 'MDK', name: 'Medak' },
-    { code: 'RNG', name: 'Rangareddy' },
-    { code: 'MHB', name: 'Mahbubnagar' },
-    { code: 'ADL', name: 'Adilabad' },
-    { code: 'KMM', name: 'Khammam' },
-    { code: 'NLG', name: 'Nalgonda' }
+    { code: 'ADB', name: 'Adilabad' },
+{ code: 'BHK', name: 'Bhadradri Kothagudem' },
+{ code: 'HNK', name: 'Hanumakonda' },
+{ code: 'HYD', name: 'Hyderabad' },
+{ code: 'JGT', name: 'Jagtial' },
+{ code: 'JGN', name: 'Jangoan' },
+{ code: 'JAY', name: 'Jayashankar Bhoopalpally' },
+{ code: 'JGL', name: 'Jogulamba Gadwal' },
+{ code: 'KMR', name: 'Kamareddy' },
+{ code: 'KNR', name: 'Karimnagar' },
+{ code: 'KMM', name: 'Khammam' },
+{ code: 'KBA', name: 'Komaram Bheem Asifabad' },
+{ code: 'MBD', name: 'Mahabubabad' },
+{ code: 'MBN', name: 'Mahabubnagar' },
+{ code: 'MCL', name: 'Mancherial' },
+{ code: 'MDK', name: 'Medak' },
+{ code: 'MMK', name: 'Medchal-Malkajgiri' },
+{ code: 'MLG', name: 'Mulugu' },
+{ code: 'NGK', name: 'Nagarkurnool' },
+{ code: 'NGD', name: 'Nalgonda' },
+{ code: 'NPT', name: 'Narayanpet' },
+{ code: 'NRM', name: 'Nirmal' },
+{ code: 'NZB', name: 'Nizamabad' },
+{ code: 'PDP', name: 'Peddapalli' },
+{ code: 'RSL', name: 'Rajanna Sircilla' },
+{ code: 'RRD', name: 'Rangareddy' },
+{ code: 'SGD', name: 'Sangareddy' },
+{ code: 'SDP', name: 'Siddipet' },
+{ code: 'SRY', name: 'Suryapet' },
+{ code: 'VKB', name: 'Vikarabad' },
+{ code: 'WNP', name: 'Wanaparthy' },
+{ code: 'WRG', name: 'Warangal' },
+{ code: 'YBG', name: 'Yadadri Bhuvanagiri' }
   ],
   AP: [
-    { code: 'VZG', name: 'Visakhapatnam' },
-    { code: 'VJA', name: 'Vijayawada' },
-    { code: 'GTR', name: 'Guntur' },
-    { code: 'NLR', name: 'Nellore' },
-    { code: 'KNL', name: 'Kurnool' },
-    { code: 'TRP', name: 'Tirupati' },
-    { code: 'KKD', name: 'Kakinada' },
-    { code: 'RJM', name: 'Rajahmundry' },
-    { code: 'ANT', name: 'Anantapur' },
-    { code: 'CDR', name: 'Chittoor' }
+    { code: 'PMY', name: 'Parvathipuram Manyam' },
+{ code: 'VSP', name: 'Visakhapatnam' },
+{ code: 'VZM', name: 'Vizianagaram' },
+{ code: 'AKP', name: 'Anakapalli' },
+{ code: 'KAK', name: 'Kakinada' },
+{ code: 'KSM', name: 'Konaseema' },
+{ code: 'ASR', name: 'Alluri Sitaramaraju' },
+{ code: 'EGD', name: 'East Godavari' },
+{ code: 'ELR', name: 'Eluru' },
+{ code: 'NTR', name: 'NTR District' },
+{ code: 'GTR', name: 'Guntur' },
+{ code: 'WGD', name: 'West Godavari' },
+{ code: 'BPT', name: 'Bapatla' },
+{ code: 'PLD', name: 'Palnadu' },
+{ code: 'SPS', name: 'Sri Potti Sriramulu Nellore' },
+{ code: 'PKM', name: 'Prakasam' },
+{ code: 'TRP', name: 'Tirupati' },
+{ code: 'AMY', name: 'Annamayya' },
+{ code: 'YSR', name: 'YSR Kadapa' },
+{ code: 'CTR', name: 'Chittoor' },
+{ code: 'ATP', name: 'Anantpur' },
+{ code: 'KNL', name: 'Kurnool' },
+{ code: 'SSS', name: 'Sri Satyasai' },
+{ code: 'NDL', name: 'Nandyal' },
+{ code: 'SBD', name: 'Sri. Balaji Dist' }
   ]
 };
 
 export const divisions = {
-  HYD: [
+ HYD: [
     { code: 'SEC', name: 'Secunderabad' },
     { code: 'LBN', name: 'LB Nagar' },
     { code: 'KPH', name: 'Kukatpally' },
@@ -94,7 +132,241 @@ export const divisions = {
     { code: 'GDR', name: 'Gudivada' },
     { code: 'MKL', name: 'Machilipatnam' },
     { code: 'NNR', name: 'Nandigama' }
-  ]
+  ],
+  ADL: [
+    { code: 'ADL1', name: 'Adilabad Division 1' },
+    { code: 'ADL2', name: 'Adilabad Division 2' }
+  ],
+  BHK: [
+    { code: 'BHK1', name: 'Bhadradri Kothagudem Division 1' },
+    { code: 'BHK2', name: 'Bhadradri Kothagudem Division 2' }
+  ],
+  JGT: [
+    { code: 'JGT1', name: 'Jagtial Division 1' },
+    { code: 'JGT2', name: 'Jagtial Division 2' }
+  ],
+  JGN: [
+    { code: 'JGN1', name: 'Jangaon Division 1' },
+    { code: 'JGN2', name: 'Jangaon Division 2' }
+  ],
+  JBP: [
+    { code: 'JBP1', name: 'Jayashankar Bhoopalpally Division 1' },
+    { code: 'JBP2', name: 'Jayashankar Bhoopalpally Division 2' }
+  ],
+  JGD: [
+    { code: 'JGD1', name: 'Jogulamba Gadwal' },
+    { code: 'JGD2', name: 'Allampur' }
+  ],
+  KMR: [
+    { code: 'KMR', name: 'Kamareddy' },
+{ code: 'BSW', name: 'Banswada' },
+{ code: 'YLD', name: 'Yellareddy' }
+  ],
+  KNR: [
+   { code: 'KNR', name: 'Karimnagar' },
+{ code: 'HZB', name: 'Huzurabad' }
+  ],
+  KMM: [
+    { code: 'KMM', name: 'Khammam' },
+{ code: 'KLR', name: 'Kallur' }
+  ],
+  KBA: [
+    { code: 'ASF', name: 'Asifabad' },
+{ code: 'KGN', name: 'Kagaznagar' }
+  ],
+  MBD: [
+    { code: 'MBD', name: 'Mahabubabad' },
+{ code: 'THR', name: 'Thorrur' }
+  ],
+  MBN: [
+    { code: 'MBN1', name: 'Mahabubnagar Division 1' },
+    { code: 'MBN2', name: 'Mahabubnagar Division 2' }
+  ],
+  MCL: [
+    { code: 'MCL', name: 'Mancherial' },
+{ code: 'BLY', name: 'Bellampally' }
+  ],
+  MDK: [
+    { code: 'MDK', name: 'Medak' },
+{ code: 'NSP', name: 'Narsapur' },
+{ code: 'TPR', name: 'Toopran' }
+  ],
+  MMG: [
+    { code: 'KES', name: 'Keesara' },
+{ code: 'MLK', name: 'Malkajgiri' }
+  ],
+  MLG: [
+    { code: 'MLG1', name: 'Mulugu Division 1' },
+    { code: 'MLG2', name: 'Mulugu Division 2' }
+  ],
+  NGK: [
+    { code: 'NGK', name: 'Nagarkurnool' },
+{ code: 'ACP', name: 'Achampet' },
+{ code: 'KLK', name: 'Kalwakurthy' },
+{ code: 'KLP', name: 'Kollapur' }
+  ],
+  NGD: [
+    { code: 'NGD', name: 'Nalgonda' },
+{ code: 'MRG', name: 'Miryalaguda' },
+{ code: 'DVR', name: 'Devarakonda' },
+{ code: 'CND', name: 'Chandur' }
+  ],
+  NPT: [
+    { code: 'NPT', name: 'Narayanpet' },
+    { code: 'NPT2', name: 'Narayanpet Division 2' }
+  ],
+  NRM: [
+    { code: 'NRM', name: 'Nirmal' },
+{ code: 'BNS', name: 'Bhainsa' }
+  ],
+  NZB: [
+    { code: 'NZB', name: 'Nizamabad' },
+{ code: 'BDN', name: 'Bodhan' },
+{ code: 'ARM', name: 'Armoor' }
+  ],
+  PDP: [
+   { code: 'PDP', name: 'Peddapalli' },
+{ code: 'MNT', name: 'Manthani' }
+  ],
+  RSL: [
+    { code: 'RSL', name: 'Sircilla' },
+{ code: 'VML', name: 'Vemulawada' }
+  ],
+  RRD: [
+   { code: 'CHV', name: 'Chevella' },
+{ code: 'IBR', name: 'Ibrahimpatnam' },
+{ code: 'RJN', name: 'Rajendranagar' },
+{ code: 'SHD', name: 'Shadnagar' },
+{ code: 'KND', name: 'Kandukur' }
+
+  ],
+  SGD: [
+    { code: 'SGD', name: 'Sangareddy' },
+{ code: 'NKD', name: 'Narayankhed' },
+{ code: 'ZBD', name: 'Zaheerabad' },
+{ code: 'JPT', name: 'Jogipet' }
+  ],
+  SDP: [
+{ code: 'SDP', name: 'Siddipet' },
+{ code: 'GJW', name: 'Gajwel' },
+{ code: 'HSB', name: 'Husnabad' }
+  ],
+  SRY: [
+    { code: 'SRY', name: 'Suryapet' },
+{ code: 'KOD', name: 'Kodad' }
+  ],
+  VKB: [
+    { code: 'VKB', name: 'Vikarabad' },
+{ code: 'TND', name: 'Tandur' }
+  ],
+  WNP: [
+    { code: 'WRG', name: 'Warangal' },
+{ code: 'NSP', name: 'Narsampet' }
+  ],
+  YBG: [
+    { code: 'BGR', name: 'Bhongir' },
+{ code: 'CHT', name: 'Choutuppal' }
+  ],
+  PVM: [
+    { code: 'PVM1', name: 'Parvathipuram Manyam Division 1' },
+    { code: 'PVM2', name: 'Parvathipuram Manyam Division 2' }
+  ],
+  VSP: [
+    { code: 'VSP1', name: 'Visakhapatnam Division 1' },
+    { code: 'VSP2', name: 'Visakhapatnam Division 2' }
+  ],
+  VZM: [
+    { code: 'VZM1', name: 'Vizianagaram Division 1' },
+    { code: 'VZM2', name: 'Vizianagaram Division 2' }
+  ],
+  ANK: [
+    { code: 'ANK1', name: 'Anakapalli Division 1' },
+    { code: 'ANK2', name: 'Anakapalli Division 2' }
+  ],
+  KAK: [
+    { code: 'KAK1', name: 'Kakinada Division 1' },
+    { code: 'KAK2', name: 'Kakinada Division 2' }
+  ],
+  KSM: [
+    { code: 'KSM1', name: 'Konaseema Division 1' },
+    { code: 'KSM2', name: 'Konaseema Division 2' }
+  ],
+  ASR: [
+    { code: 'ASR1', name: 'Alluri Sitaramaraju Division 1' },
+    { code: 'ASR2', name: 'Alluri Sitaramaraju Division 2' }
+  ],
+  EGD: [
+    { code: 'EGD1', name: 'East Godavari Division 1' },
+    { code: 'EGD2', name: 'East Godavari Division 2' }
+  ],
+  ELR: [
+    { code: 'ELR1', name: 'Eluru Division 1' },
+    { code: 'ELR2', name: 'Eluru Division 2' }
+  ],
+  NTR: [
+    { code: 'NTR1', name: 'NTR District Division 1' },
+    { code: 'NTR2', name: 'NTR District Division 2' }
+  ],
+  GTR: [
+    { code: 'GTR1', name: 'Guntur Division 1' },
+    { code: 'GTR2', name: 'Guntur Division 2' }
+  ],
+  WGD: [
+    { code: 'WGD1', name: 'West Godavari Division 1' },
+    { code: 'WGD2', name: 'West Godavari Division 2' }
+  ],
+  BPT: [
+    { code: 'BPT1', name: 'Bapatla Division 1' },
+    { code: 'BPT2', name: 'Bapatla Division 2' }
+  ],
+  PLN: [
+    { code: 'PLN1', name: 'Palnadu Division 1' },
+    { code: 'PLN2', name: 'Palnadu Division 2' }
+  ],
+  NLR: [
+    { code: 'NLR1', name: 'Sri Potti Sriramulu Nellore Division 1' },
+    { code: 'NLR2', name: 'Sri Potti Sriramulu Nellore Division 2' }
+  ],
+  PKM: [
+    { code: 'PKM1', name: 'Prakasam Division 1' },
+    { code: 'PKM2', name: 'Prakasam Division 2' }
+  ],
+  TPT: [
+    { code: 'TPT1', name: 'Tirupati Division 1' },
+    { code: 'TPT2', name: 'Tirupati Division 2' }
+  ],
+  AMY: [
+    { code: 'AMY1', name: 'Annamayya Division 1' },
+    { code: 'AMY2', name: 'Annamayya Division 2' }
+  ],
+  YKD: [
+    { code: 'YKD1', name: 'YSR Kadapa Division 1' },
+    { code: 'YKD2', name: 'YSR Kadapa Division 2' }
+  ],
+  CTR: [
+    { code: 'CTR1', name: 'Chittoor Division 1' },
+    { code: 'CTR2', name: 'Chittoor Division 2' }
+  ],
+  ATP: [
+    { code: 'ATP1', name: 'Anantapur Division 1' },
+    { code: 'ATP2', name: 'Anantapur Division 2' }
+  ],
+  KNL: [
+    { code: 'KNL1', name: 'Kurnool Division 1' },
+    { code: 'KNL2', name: 'Kurnool Division 2' }
+  ],
+  STS: [
+    { code: 'STS1', name: 'Sri Satyasai Division 1' },
+    { code: 'STS2', name: 'Sri Satyasai Division 2' }
+  ],
+  NDL: [
+    { code: 'NDL1', name: 'Nandyal Division 1' },
+    { code: 'NDL2', name: 'Nandyal Division 2' }
+  ],
+  SBD: [
+    { code: 'SBD1', name: 'Sri Balaji Dist Division 1' },
+    { code: 'SBD2', name: 'Sri Balaji Dist Division 2' }
+  ],
 };
 
 export const mandals = {
